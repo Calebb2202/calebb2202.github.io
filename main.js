62e127f0b56e6799fbd862cc3c3f03288f9477a7
Caleb Boerma <calebb2202@gmail.com>
2026-05-18 16:51:20 -0400

    Finished new projects section look and began dynamic sizing

--- main.js
+++ main.js
@@ -2,11 +2,16 @@
 const projectDescriptions = document.getElementsByClassName("project-description")
 for (let i = 0; i < projectDescriptions.length; i++){
     let textLength = projectDescriptions[i].querySelector('p').textContent.length;
-    projectDescriptions[i].querySelector('p').style.lineHeight = 500 / textLength
+    projectDescriptions[i].querySelector('p').style.lineHeight = 500 / textLength;
 }
 
-// make experience arrow height
-const arrowLine = document.getElementById("arrow-line")
-const experiencesContainer = document.getElementById("experiences-container")
-arrowLine.style.height = `${experiencesContainer.offsetHeight - 20}px`
+function onResize() {
+    // make experience arrow height
+    const arrowLine = document.getElementById("arrow-line")
+    const experiencesContainer = document.getElementById("experiences-container")
+    arrowLine.style.height = 0;
+    arrowLine.style.height = `${experiencesContainer.offsetHeight - 50}px`
+}
 
+onResize()
+window.addEventListener('resize', onResize)
